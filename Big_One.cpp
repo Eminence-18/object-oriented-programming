46bb8d1e026812c91f47268152b3cf868825e243
Eminence-18 <2406531@students.kcau.ac.ke>
2025-11-16 22:17:12 +0300

    Add files via upload

---
+++ Big_One.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main( )
+{
+	int a,b;
+
+	cout<<"Enter two integers: ";
+	cin>>a>>b;
+
+	if (a == b)
+		cout<<"\n"<<a<<" is equal "<<b;
+
+	if (a > b)
+		cout<<"\n"<<a<<" is larger than "<<b;
+
+	else if (a < b)
+		cout<<"\n"<<b<<" is larger than "<<a;
+
+	cout<<"\n\n";
+
+	return 0;
+}
